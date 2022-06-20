f4097796af5928bfd0c7069f5c9df4561e413645
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-06-20 15:55:37 -0300

    Feat: Cria a branch do Requisito 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Inicia o projeto
 function App() {
   return (
     <div className="meals">
