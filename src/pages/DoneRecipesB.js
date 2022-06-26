5865ba34c31a0c0efa13d3ecb13ca2626ec49c70
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-06-26 19:02:16 -0300

    Fix: Resolve a lógica do Requisito 59

--- src/pages/DoneRecipesB.js
+++ src/pages/DoneRecipesB.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
@@ -7,11 +8,22 @@ function DoneRecipesB({ index, recipe }) {
   // console.log(recipe, index);
   const [message, setMessage] = useState('');
   const link = `${window.location.origin}/${recipe.type}s/${recipe.id}`;
+  const history = useHistory();
 
   const HandleSubmit = (e) => {
     e.preventDefault();
     copy(link);
     setMessage('Link copied!');
+  };
+
+  const handleNavigate = (recipeClicked) => {
+    if (recipeClicked.type === 'food') {
+      history.push(`/foods/${recipe.id}`);
+    }
+
+    if (recipeClicked.type === 'drink') {
+      history.push(`/drinks/${recipe.id}`);
+    }
   };
 
   return (
@@ -24,16 +36,30 @@ function DoneRecipesB({ index, recipe }) {
                 src={ recipe.image }
                 alt="Done Recipe"
                 data-testid={ `${index}-horizontal-image` }
+                onClick={ () => handleNavigate(recipe) }
+                aria-hidden="true"
               />
               <h3 data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</h3>
-              <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+              <h2
+                data-testid={ `${index}-horizontal-name` }
+                onClick={ () => handleNavigate(recipe) }
+                aria-hidden="true"
+              >
+                {recipe.name}
+
+              </h2>
               <p>{recipe.nationality}</p>
               <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-              <p
-                data-testid={ `${index}-horizontal-tag` }
-              >
-                { [recipe.tags.split(',').slice(0, 2)] }
-              </p>
+              {
+                recipe.tags !== ''
+                  && (
+                    <p
+                      data-testid={ `${index}-horizontal-tag` }
+                    >
+                      { [recipe.tags.split(',').slice(0, 2)] }
+                    </p>
+                  )
+              }
               <button
                 type="submit"
                 data-testid={ `${index}-horizontal-share-btn` }
@@ -50,9 +76,18 @@ function DoneRecipesB({ index, recipe }) {
                 src={ recipe.image }
                 alt="Done Recipe"
                 data-testid={ `${index}-horizontal-image` }
+                onClick={ () => handleNavigate(recipe) }
+                aria-hidden="true"
               />
               <h3>{recipe.alcoholicOrNot}</h3>
-              <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+              <h2
+                data-testid={ `${index}-horizontal-name` }
+                onClick={ () => handleNavigate(recipe) }
+                aria-hidden="true"
+              >
+                {recipe.name}
+
+              </h2>
               <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
               <button
                 type="submit"
