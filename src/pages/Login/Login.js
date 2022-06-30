179f1029bfff236ddc8423e0bc4d090111d17ba3
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-06-30 16:26:29 -0300

    Fix: Muda o estilo do CSS da tela de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import saveMealsToken from '../../helpers/saveMealsToken';
 import saveCocktailsToken from '../../helpers/saveCocktailsToken';
-import { Div, DivGlobal } from './Style';
+import './Login.css';
 
 function Login() {
   const history = useHistory();
   const [user, setUser] = useState({ email: '', password: '' });
-
   const isPlayButtonDisabled = () => {
     const EMAIL_LENGH = 6;
     if (user.email.includes('@') && user.email.includes('.com')
@@ -16,11 +15,9 @@ function Login() {
     }
     return true;
   };
-
   const HandleChange = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
-
   const HandleSubmit = (e) => {
     e.preventDefault();
     saveMealsToken();
@@ -28,47 +25,46 @@ function Login() {
     localStorage.setItem('user', JSON.stringify({ email: user.email }));
     history.push('/foods');
   };
-
   return (
-    <DivGlobal>
-      <Div>
-        <form>
+    <div className="backGroundLogin">
+      <form className="formStyle">
+        <div className="inputContainer">
           <label htmlFor="email">
-            Email
             <input
+              className="input"
               id="email"
               data-testid="email-input"
               name="email"
               value={ user.email }
               type="text"
-              placeholder="Email"
+              placeholder="Insira seu e-mail"
               onChange={ HandleChange }
             />
           </label>
           <label htmlFor="passwordLabel">
-            Password
             <input
+              className="input"
               id="passwordLabel"
               data-testid="password-input"
               name="password"
               value={ user.password }
               type="password"
-              placeholder="Password"
+              placeholder="Insira sua senha"
               onChange={ HandleChange }
             />
           </label>
-          <button
-            data-testid="login-submit-btn"
-            type="submit"
-            disabled={ isPlayButtonDisabled() }
-            onClick={ HandleSubmit }
-          >
-            Enter
-          </button>
-        </form>
-      </Div>
-    </DivGlobal>
+        </div>
+        <button
+          className="btn btn-dark px-4 mb-2"
+          data-testid="login-submit-btn"
+          type="submit"
+          disabled={ isPlayButtonDisabled() }
+          onClick={ HandleSubmit }
+        >
+          Enter
+        </button>
+      </form>
+    </div>
   );
 }
-
 export default Login;
